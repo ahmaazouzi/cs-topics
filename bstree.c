6903f4625ac57756fcdf5ebd24c88bf48678784d
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2019-01-02 23:34:48 -0500

    function pointer example

--- bstree.c
+++ bstree.c
@@ -1,54 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct node{
-	char data;
-	struct node *left, *right;
-} node;
+// typedef struct node {
+// 	char data;
+// 	struct node *left, *right;
+// } node;
 
-node *newNode(char data);
-node *insert(node *node, char data);
-void inorder(node * node);
-int main(){
-	node *root = NULL; 
-	root = insert(root, 'd');
-	insert(root, 'z');
-	insert(root, 'z');
-	insert(root, 'z');
-	insert(root, 'z');
-	insert(root, 'z');
-	insert(root, 'z');
-	insert(root, 'a');
-	insert(root, 'z');
-	insert(root, '#');
-	insert(root, 'l');
-	inorder(root);
-	return 0;
-}
+// int main(){
+// 	return 0;
+// }
 
-node *newNode(char data){
-	node *node = (struct node *) malloc(sizeof(struct node));
-	node->data = data;
-	node->left = node->right = NULL;
-	return node;
-}
-
-void inorder(node *node){
-	if (node != NULL){
-		inorder(node->left);
-		printf("%c ", node->data);
-		inorder(node->right);
-	}
-}
-
-node *insert(node *node, char data){
-	if (node == NULL)
-		return newNode(data);
-	// if (node->data == data)
-	// 	;
-	if (data < node->data)
-		node->left = insert(node->left, data);
-	else if (data > node->data)
-		node->right = insert(node->right, data);
-	return node;
-}
+// inorder()
