7ed5df25d43e334805000e7563c12eaf6a4b6871
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2019-01-03 23:19:32 -0500

    bored

--- bstree.c
+++ bstree.c
@@ -2,13 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-// typedef struct node {
-// 	char data;
-// 	struct node *left, *right;
-// } node;
+typedef struct node {
+	char data;
+	struct node *left, *right;
+} node;
 
-// int main(){
-// 	return 0;
-// }
+node *newNode(char data);
+node *insert(node *, char);
+void inorder(node *);
+int main(){
+	node *root = NULL;
+	root = insert(root, 100);
+	insert(root, 101);
+	insert(root, 99);
+	insert(root, 98);
+	insert(root, 119);
+	inorder(root);
+	return 0;
+}
 
-// inorder()
+node *newNode(char data){
+	node *node = (struct node *) malloc(sizeof(struct node));
+	node->data = data;
+	node->left = node->right = NULL;
+	return node;
+}
+
+node *insert(node * node, char data){
+	if (node == NULL)
+		return newNode(data);
+	if (data < node->data)
+		node->left = insert(node->left, data);
+	else if(data > node->data)
+		node->right = insert(node->right, data);
+	return node;
+}
+
+void inorder(node *node){
+	if(node != NULL){
+		inorder(node->left);
+		printf("%c\n", node->data);
+		inorder(node->right);
+	}
+}
+
+node *delete(node *node){
+	return *node;
+}
