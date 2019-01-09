174563f03787bac4552d533d184b575e23b3c201
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2019-01-09 02:19:51 -0500

    build heap

--- heapsort.py
+++ heapsort.py
@@ -9,17 +9,19 @@ def get_left(index):
 def get_right(index):
 	return (2 * index) + 1
 
+#Repackeging this spaghetti into a heap object will make it less messy
 def max_heapify(alist, index):
 	left = get_left(index)
 	right = get_right(index)
 
 	# heap size (to be changed)
-	if left <= len(alist) and alist[left] > alist[index]:
+	if left < len(alist) and alist[left] > alist[index]:
 		largest = left
 	else:
 		largest = index
-	if right <= len(alist) and alist[right] > alist[largest]:
+	if right < len(alist)  and alist[right] > alist[largest]:
 		largest = right
+
 	if largest != index:
 		# pythonism can replace this mess
 		temp = alist[largest]
@@ -27,10 +29,23 @@ def max_heapify(alist, index):
 		alist[index] = temp
 		max_heapify(alist, largest)
 
-a = [1, 16, 14, 10, 8, 7, 9, 3, 2, 4]
-print a
+def build_max_heap(alist):
+	# Crap to be fixed
+	half_size = reversed(range((len(alist))/2))
+	for i in half_size:
+		max_heapify(alist, i)
+		
+
+a = [1, 16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+print "1 is unheaped: ", a
 max_heapify(a, 0)
-print a
+print "1 is heaped: ", a
+print
+
+b = [4, 1, 3, 2, 16, 9, 10, 14, 8]
+print "unheapified list: ", b
+build_max_heap(b)
+print "heapified list: ", b
 
 
 
